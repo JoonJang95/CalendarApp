92d11beaee2b1719a6e28ce370e0f9a5e2703db0
JoonJang95 <joon.jang95@gmail.com>
2018-12-04 00:36:18 -0500

    Set up initial environment for YouTubeChatter App

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+//functional (stateless) component
+// const App = () => {
+//   return <div> I am now rendered</div>;
+// }
+
+
+//Class component (stateful) component
+class App extends React.Component {
+  render() {
+    return <div>I AM NOW RENDERED</div>
+  }
+}
+
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+//The purpose of this file is to serve as the entry point for our application
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('App'));
