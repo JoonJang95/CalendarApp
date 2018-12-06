fa6ef15b6af1d35d18e04451ce5e7282766650b7
JoonJang95 <joon.jang95@gmail.com>
2018-12-06 01:14:27 -0500

    Add more practice notes

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,61 @@ import React from 'react';
 //   return <div> I am now rendered</div>;
 // }
 
-
 //Class component (stateful) component
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'It\'s Working'
+    }
+  }
+
   render() {
-    return <div>I AM NOW RENDERED</div>
+    return (
+    <div>
+      <div>{this.state.text}</div>
+    </div>
+    );
   }
 }
 
 
-
 export default App;
+
+
+
+/*
+Practice Example
+
+Goal => Have input box display the text as we type
+  -> On clicking bold, make the data display BOLD
+  1. Make an event handler -> onChange (key-stroke changes) that runs function handleInputChange (function we make);
+      => handleInputChange => take our keystrokes and save data in state
+*/
+
+//A little input example
+/*
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      text: e.target.values
+    });
+  }
+
+  render() {
+    return (
+    <div>
+      <input onChange={this.handleInputChange} />
+      <div>{this.state.text}</div>
+    </div>
+    );
+  }
+}
+*/
